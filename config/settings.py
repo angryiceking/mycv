1567f339eed2135dc972ab4dd49ff3a693849dfb
Carl <carl.almayda@stratodigital.io>
2020-11-22 19:32:10 +0800

    Add app to settings.py, and updated index.html

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o)hakhm26z@tt$9(p(-ay679bf7&4qy*+olb++xt$cdsil9#ht'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app.resume',
 ]
 
 MIDDLEWARE = [
